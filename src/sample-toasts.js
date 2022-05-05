f16a5fc2dda52c61479ef63dd99a9761ffaec34e
Kamil <kamilschmidt06@gmail.com>
2022-05-05 22:00:05 +0200

    :bulb:  added sample toasts

--- src/sample-toasts.js
+++ src/sample-toasts.js
@@ -2,72 +2,72 @@
 const toasts = {
     toast1: {
         name: "Classic Toast",
-        image: "/images/hali.jpg",
+        image: "/images/classic.png",
         desc:
             "Everyone’s favorite classic toast with egg and cheese inside",
-        price: 499,
+        price: 300,
         status: "available"
     },
 
     toast2: {
         name: "Beacon Toast",
-        image: "/images/lobster.jpg",
+        image: "/images/beacon.png",
         desc:
             "Upgraded version of classic toast with two pieces of beacon made on chips .",
-        price: 799,
+        price: 500,
         status: "available"
     },
 
     toast3: {
         name: "Double Cheese Toast",
-        image: "/images/scallops.jpg",
+        image: "/images/double.png",
         desc:
             "You will find inside a two kinds of cheeses chosen by yourself",
-        price: 599,
+        price: 400,
         status: "unavailable"
     },
 
     toast4: {
         name: "King Toast",
-        image: "/images/mahi.jpg",
+        image: "/images/king.png",
         desc:
             "The King of all toasts is big enough to make you full for a rest of day. You will find inside a double beacon toast.",
-        price: 1499,
+        price: 500,
         status: "available"
     },
 
     toast5: {
         name: "Giant Egg Toast",
-        image: "/images/crab.jpg",
+        image: "/images/giant.png",
         desc:
             "Toast made with grilled mozzarella cheese inside and beacon",
-        price: 1000,
+        price: 300,
         status: "available"
     },
 
     toast6: {
         name: "Korean Toast Master",
-        image: "/images/salmon.jpg",
+        image: "/images/sausage.png",
         desc:
             "Sausage pizza toast made with a lot of meat",
-        price: 1800,
+        price: 350,
         status: "available"
     },
 
     toast7: {
         name: "Fruit Toast",
-        image: "/images/oysters.jpg",
-        desc: "Toast made with Nutella, Strawberries, coconut pieces ",
-        price: 1500,
+        image: "/images/fruit.png",
+        desc: "Toast served with a lot of natural vitamins ",
+        price: 700,
         status: "available"
     },
 
     toast8: {
         name: "Friendly Coffee",
-        image: "/images/mussels.jpg",
+        image: "/images/coffe.png",
         desc:
             "",
-        price: 425,
+        price: 450,
         status: "available"
     },
 };
